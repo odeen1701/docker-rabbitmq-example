ddb7fed89e9e7efd4db788479fe618bbb2f1918d
Daniel <odeen1701@gmail.com>
2021-02-23 10:01:27 +0000

    now queue has taking care of messages non processed will not be acknowledged.

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
-import pika, sys, os
+import pika, sys, os, time
 
 def main():
     credentials = pika.PlainCredentials('guest', 'guest')
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', credentials=credentials))
     channel = connection.channel()
 
-    channel.queue_declare(queue='busqueda_medios')
+    channel.queue_declare(queue='busqueda_medios_durable', durable=True)
 
     def iniciar_busqueda(ch, method, properties, body):
-        print("starting search for:")
-        print(ch)
-        print(method)
-        print(properties)
         print(" [x] Received %r" % body)
+        print("stopping for 3 seconds")
+        time.sleep(3)
+        ch.basic_ack(delivery_tag = method.delivery_tag)
 
-    channel.basic_consume(queue='busqueda_medios', on_message_callback=iniciar_busqueda, auto_ack=True)
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='busqueda_medios', on_message_callback=iniciar_busqueda)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
--- publisher/publisher.py
+++ publisher/publisher.py
@@ -5,8 +5,9 @@ credentials = pika.PlainCredentials('guest', 'guest')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', credentials=credentials))
 channel = connection.channel()
 
-channel.queue_declare(queue='busqueda_medios')
+channel.queue_declare(queue='busqueda_medios_durable', durable=True)
+for i in range(30):
+    channel.basic_publish(exchange='', routing_key='busqueda_medios', body='-->xx {} xx<--'.format(i), properties=pika.BasicProperties(delivery_mode=2)) #delivery_mode=2 -> message is persistent
+    print(" [x] Sent search request'")
 
-channel.basic_publish(exchange='', routing_key='busqueda_medios', body='{"ambito":"CAM","medios":"todos","claves":"todas","username":"odeen","client","Cntest"}')
-print(" [x] Sent search request'")
 connection.close()
